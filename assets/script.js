c71bc4db882b6ef968e59d4c13574ac4cd0a3d27
HacAtac <jhack00@icloud.com>
2021-08-25 22:52:22 -0500

    fixed css link. added function/prompt for pass/length. fixed generatePassword is not defined when trying to run pass/length function

--- assets/script.js
+++ assets/script.js
@@ -6,8 +6,22 @@ var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerChar = "abcdefghijklmnopqrstuvwxyz";
 var passwordLength = "";
 //When I click the button it needs to prompt for password critera
+function chooseLength() {
+  passwordLength = prompt("How many characters?");
+  if (passwordLength < 8) {
+      alert("Please make a valid selection!");
+      chooseLength();
+  }else if (passwordLength > 128) {
+    alert("Please make a valid selection!");
+  }
+  return passwordLength;
+}
 //When prompted for password criteria then you select which criteria to include in password
 //When prompted for the length of password then you choose betwee 8 and 128
+function generatePassword() {
+  chooseLength();
+  console.log(passwordLength);
+}
 //when asked for ch types to include in pass then you confirm wheter or not to include lower,upper,number,special chars
 //when you answer each prompt then your input should be validated and at least one char type be seleceted
 //when all prompts are answered thena  password is generated that matches the selected criteria
